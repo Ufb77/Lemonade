bb3aba462d38ec13b7ad51becfb05f7c910d2f7b
Jose Antonio Fdez-Montes Garcia <jafmgdam@gmail.com>
2023-12-03 19:19:28 +0100

    V_Fin_No_Warnings

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -18,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -81,7 +76,7 @@ fun MainColumn(modifier:Modifier = Modifier){
     //Establece el número de clicks necesarios para cambiar de imagen e inicializa la variable
     //click para contar el número de clicks que hace el usuario
     var click = 0
-    var numeroClicks = (2..4).random()
+    val numeroClicks = (2..4).random()
 
     //Los textos, imagenes y descripción se asignan en función de la "etapa" en la que esté
     //la aplicación
@@ -125,7 +120,7 @@ fun MainColumn(modifier:Modifier = Modifier){
         //coincidie con el valor aleatorio de número de clicks para cambiar la imagen. En caso positivo,
         //la imagen cambia.
         //Si se encuenta en cualquier otro paso, se pasa a la siguiente imagen.
-        //Si algo falla, se pasa otra vez a la primera imagen
+        //En caso de alcanzar el límite, se pasa otra vez a la primera imagen
         Button(onClick = {
             if(resultado == 2){
                 click ++
